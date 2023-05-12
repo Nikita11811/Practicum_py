0c1486029ec8b7ac7168b6721717959e429bb13b
Nikita11811 <124414515+Nikita11811@users.noreply.github.com>
2023-05-12 22:45:24 +0300

    Delete task6 directory

--- task6/task_6.3.py
+++
@@ -1,8 +0,0 @@
-import time
-
-
-date_now = time.time()
-f = input("Введите дату рождения в формате дд-мм-гггг: ")
-date = time.mktime(time.strptime(str(f), "%d-%m-%Y"))
-print("Прожито дней:", int((date_now - date) / 86400))
-
