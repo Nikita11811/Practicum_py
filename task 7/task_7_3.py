d882f6c09ebce0c34b15e6c2f430f9f419cabc30
Nikita11811 <124414515+Nikita11811@users.noreply.github.com>
2023-05-27 07:50:39 +0300

    Update task_7_3.py

--- task 7/task_7_3.py
+++ task 7/task_7_3.py
@@ -8,7 +8,8 @@ text = time.strftime('%H-%M-%S_%d-%m-%Y')
 
 def create_folder(a):
     os.mkdir(a)
-    with open(r'C:\Users\Пользователь\Desktop\Новая папка\STUDY\1\1.txt', "w") as f:
+    file_name = a + '/1.txt'
+    with open(file_name, "w") as f:
         f.write(text)
 
 
