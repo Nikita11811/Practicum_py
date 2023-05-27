28a8d6c40a2479ae3d1b174949553f28cc003712
Nikita11811 <124414515+Nikita11811@users.noreply.github.com>
2023-05-27 06:19:31 +0300

    Update task_7_3.py

--- task 7/task_7_3.py
+++ task 7/task_7_3.py
@@ -3,25 +3,28 @@ import os
 import time
 
 
-def CreateFolder(a):
+text = time.strftime('%H-%M-%S_%d-%m-%Y')
+
+
+def create_folder(a):
     os.mkdir(a)
-    with open(r'C:\Users\test\Desktop\STUDY\1\1.txt', "w") as text:
-        text.write(time.strftime('%H-%M-%S_%d-%m-%Y'))
+    with open(r'C:\Users\Пользователь\Desktop\Новая папка\STUDY\1\1.txt', "w") as f:
+        f.write(text)
 
 
 class TestMethod3(unittest.TestCase):
 
     def setUp(self):
-        self.create = CreateFolder(r'C:\Users\test\Desktop\STUDY\1')
+        self.create = create_folder(r'C:\Users\Пользователь\Desktop\Новая папка\STUDY\1')
 
     def test_file(self):
-        self.assertIsNotNone(r'C:\Users\test\Desktop\STUDY\1\1.txt')
+        with open ('C:/Users/Пользователь/Desktop/Новая папка/STUDY/1/1.txt') as f:
+            self.assertEqual(text, f.read())
 
     def tearDown(self):
-       os.remove(r'C:\Users\test\Desktop\STUDY\1\1.txt')
-       os.rmdir(r'C:\Users\test\Desktop\STUDY\1')
+       os.remove(r'C:\Users\Пользователь\Desktop\Новая папка\STUDY\1\1.txt')
+       os.rmdir(r'C:\Users\Пользователь\Desktop\Новая папка\STUDY\1')
 
 
 if __name__ == '__main__':
     unittest.main()
-
